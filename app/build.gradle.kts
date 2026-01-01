ed2e0e4187701d647f46f90666fa2d5e6fcbed4a
Alexis Drai <drai.alexis@gmail.com>
2026-01-01 16:25:18 +0100

    📖 Improve CI (KISS & FOSS)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,9 +40,9 @@ android {
         minSdk = 24
         targetSdk = 36
         // don't update manually, use the dedicated gitlab job instead ("scheduled" manual job)
-        versionCode = 14
+        versionCode = 15
         // don't update manually, use the dedicated gitlab job instead ("scheduled" manual job)
-        versionName = "1.3.0"
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
