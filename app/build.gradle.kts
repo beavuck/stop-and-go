24b84da9a04520919289c1925c98ae392bfe0340
Alexis Drai <drai.alexis@gmail.com>
2026-03-11 19:13:39 +0100

    ⚙️ Upgrade dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,11 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import java.util.Properties
 
 plugins {
     id("com.android.application") version "8.13.2"
-    id("org.jetbrains.kotlin.android") version "2.3.0"
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0"
+    id("org.jetbrains.kotlin.android") version "2.3.10"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.10"
     id("se.patrikerdes.use-latest-versions") version "0.2.19"
     id("com.github.ben-manes.versions") version "0.53.0"
     jacoco
@@ -40,9 +41,9 @@ android {
         minSdk = 24
         targetSdk = 36
         // don't update manually, use the dedicated gitlab job instead ("scheduled" manual job)
-        versionCode = 23
+        versionCode = 24
         // don't update manually, use the dedicated gitlab job instead ("scheduled" manual job)
-        versionName = "1.4.4"
+        versionName = "1.4.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -107,8 +108,8 @@ android {
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2026.02.00")
-    val activityVersion = "1.12.2"
+    val composeBom = platform("androidx.compose:compose-bom:2026.02.01")
+    val activityVersion = "1.12.4"
     val coreKtxVersion = "1.17.0"
     val appcompatVersion = "1.7.1"
     val materialVersion = "1.13.0"
@@ -136,6 +137,20 @@ dependencies {
 }
 
 apply(from = "gradle/jacoco.gradle.kts")
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+// https://github.com/ben-manes/gradle-versions-plugin
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
+}
 
 tasks.register<Zip>("packageReleaseNativeDebugSymbols") {
     group = "packaging"
