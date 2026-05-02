d50a3e5b93e373ee95feddfe948d0977aa93f538
GitLab Command Runner <gitlab-command-runner@example.com>
2026-05-02 07:10:53 +0000

    ⚙️ Upgrade dependencies automatically

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,9 +42,9 @@ android {
         minSdk = 24
         targetSdk = 36
         // don't update manually, use the dedicated gitlab job instead ("scheduled" manual job)
-        versionCode = 30
+        versionCode = 31
         // don't update manually, use the dedicated gitlab job instead ("scheduled" manual job)
-        versionName = "1.9.0"
+        versionName = "1.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
