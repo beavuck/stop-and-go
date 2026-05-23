4c3efc623b7ec3e114b9b6a78b95470e0383034e
GitLab Command Runner <gitlab-command-runner@example.com>
2026-05-23 13:35:29 +0000

    ⚙️ Upgrade dependencies automatically

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,9 +45,9 @@ android {
         minSdk = 24
         targetSdk = 36
         // don't update manually, use the dedicated gitlab job instead ("scheduled" manual job)
-        versionCode = 32
+        versionCode = 33
         // don't update manually, use the dedicated gitlab job instead ("scheduled" manual job)
-        versionName = "1.9.2"
+        versionName = "1.9.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -113,7 +113,7 @@ android {
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2026.05.00")
+    val composeBom = platform("androidx.compose:compose-bom:2026.05.01")
     val activityVersion = "1.13.0"
     val coreKtxVersion = "1.18.0"
     val appcompatVersion = "1.7.1"
